35113d87a475f23d3c4c8e1df4b9603901f17b90
sanjinhub <sanjinhub@gmail.com>
2020-12-20 16:04:22 +0800

    添加私有APP开发

--- windows/run.py
+++ windows/run.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # encoding:utf-8
-from loguru import logger # 导入日记库，没有请先安装 pip install loguru
+from loguru import logger  # 导入日记库，没有请先安装 pip install loguru
+
 
 # 为了保证性能，使用了 async await 开启异步，Python 3.7+ 的特性
 async def hello_word(name):
     # 输出日记，生产环境会输出到指定目录
     logger.info("[Hello Word] 该 APP 执行参数为: {name}", name=name)
     # 返回值，格式为： 状态码，返回内容
-    return 0, "Hello," + name
+    return {"status": 0, "result": "Hello," + name}
 
 
 if __name__ == '__main__':
@@ -18,6 +19,7 @@ if __name__ == '__main__':
     async def test():
         status, result = await hello_word("W5")
         print(status, result)
+
 
     # 加入异步队列
     async def main(): await asyncio.gather(test())
