96dc14066926ac9105ef9ab7642db63e1b4224ad
SanJin <sanjinhub@gmail.com>
2023-02-07 10:13:37 +0800

    Update run.py

--- main/run.py
+++ main/run.py
@@ -4,7 +4,7 @@ from loguru import logger  # 导入日记库，没有请先安装 pip install lo
 
 
 # 为了保证性能，使用了 async await 开启异步，Python 3.7+ 的特性
-async def hello_word(name):
+async def hello_world(name):
     # 输出日记，生产环境会输出到指定目录
     logger.info("[Hello Word] 该 APP 执行参数为: {name}", name=name)
     # 返回值，格式为： 状态码，返回内容
